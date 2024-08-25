242d9a063d35ead076a78e2807c6c2d4ffe1323d
Zhihao Lin <zhi.lin1@outlook.com>
2024-08-26 02:04:03 +0800

    Added app to settings, fixed models, wrote tests
    Added api app to settings file. Fixed several errors within models
    migrations now work. Wrote simple test for element/landmark model.

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,19 +10,20 @@ class User(models.Model):
     
 class Site(models.Model):
     site_name = models.CharField(max_length=100)
-    city = models.charField(max_length=90)
+    city = models.CharField(max_length=90)
     country = models.CharField(max_length=30)
     description = models.CharField(max_length=2000)
     
-class Elements(models.Model):
+class Element(models.Model):
     bone_name = models.CharField(max_length=20)
-    bone_img = models.ImageField(upload_to='boneImages')
+    # bone_img = models.ImageField(upload_to='boneImages')
     
-class Landmarks(models.Model):
+class Landmark(models.Model):
     landmark_name = models.CharField(max_length=100)
-    bone = models.ForeignKey(Elements)
+    bone = models.ForeignKey(Element, on_delete=models.CASCADE)
     
-    
+    def __str__(self):
+        return self.landmark_name
 # note: still have to add spit/accno/person IDs
 # spit may have to be another model
 
@@ -48,13 +49,20 @@ class Entry(models.Model):
         CM_5_10 = "2-10cm"
         CM_10 = ">10cm"
         
-    site = models.ForeignKey(Site)
-    logged_by = models.ForeignKey(User)
-    bone = models.ForeignKey(Elements)
-    side = models.CharField(choices=Side)
-    age = models.CharField(choices=Age)
-    sex = models.CharField(choices=Sex)
-    size = models.CharField(choices=Size)
+    site = models.ForeignKey(Site, on_delete=models.RESTRICT)
+    logged_by = models.ForeignKey(User, on_delete=models.RESTRICT)
+    bone = models.ForeignKey(Element, on_delete=models.RESTRICT)
+    side = models.CharField(choices=Side, max_length=20)
+    age = models.CharField(choices=Age, max_length=50)
+    sex = models.CharField(choices=Sex, max_length=50)
+    size = models.CharField(choices=Size, max_length=50)
     generic = models.BooleanField()
     complete = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
     notes = models.CharField(max_length=2000)
+    
+    landmarks = models.ManyToManyField(Landmark, through="IDLandmarks")
+
+class IDLandmarks(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.RESTRICT)
+    landmark = models.ForeignKey(Landmark, on_delete=models.RESTRICT)
+    found = models.BooleanField(default=False)
--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from api.models import Element, Landmark, Entry, IDLandmarks
 
 # Create your tests here.
+
+class FibulaTestCase(TestCase):
+  def setUp(self):
+    Element.objects.create(bone_name="Fibula")
+    fibula = Element.objects.get(bone_name="Fibula")
+    Landmark.objects.create(landmark_name="Head", bone=fibula)
+    Landmark.objects.create(landmark_name="Neck", bone=fibula)
+    Landmark.objects.create(landmark_name="Shaft", bone=fibula)
+    Landmark.objects.create(landmark_name="Distal articular surface", bone=fibula)
+    Landmark.objects.create(landmark_name="Lateral malleolus", bone=fibula)
+
+  def testFibula(self):
+    fibula = Element.objects.get(bone_name="Fibula")
+    fibula_list = fibula.landmark_set.all()
+    print(fibula_list)
