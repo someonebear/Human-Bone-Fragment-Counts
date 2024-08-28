ba44ddd82a9c1bf9e52725aace81b6c4b300ff54
Zhihao Lin <zhi.lin1@outlook.com>
2024-08-29 01:16:46 +0800

    Simplified serializers with ModelSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,22 +1,12 @@
 from rest_framework import serializers
 from api.models import Element, Landmark
 
-class ElementSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=20)
+class ElementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Element
+        fields = ['name']
 
-    def create(self, validated_data):
-        return Element.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-
-class LandmarkSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=20)
-    name = serializers.CharField(max_length=100)
-    bone = ElementSerializer()
-
-    def create(self, validated_data):
-        return Landmark.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
+class LandmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Landmark
+        fields = ['id', 'name', 'bone']
