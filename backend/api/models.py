7d1c6c7e86ef7b1a6a20bce06ff1bc5c7d3e3f81
Zhihao Lin <zhi.lin1@outlook.com>
2024-08-29 21:07:59 +0800

    Fixed wrong human readable name on Entry fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -48,31 +48,34 @@ class Entry(models.Model):
         INFANT = "Infant"
         CHILD = "Child"
         ADOLESCENT = "Adolescent"
-        SKEL_MAT = "Skeletally Mature"
-        MAT_AD = "Mature Adult"
+        SKEL_MAT = "Skeletally Mature", "Skeletally Mature"
+        MAT_AD = "Mature Adult", "Mature Adult"
     class Sex(models.TextChoices):
         FEMALE = "Female"
         MALE = "Male"
         AMBIGUOUS = "Ambiguous"
-        NOT_ASSESS = "Not Assessed"
+        NOT_ASSESSED = "Not Assessed"
     class Size(models.TextChoices):
-        CM_2 = "< 2cm"
-        CM_2_5  = "2-5cm"
-        CM_5_10 = "2-10cm"
-        CM_10 = ">10cm"
+        CM_2 = "<2cm", "<2cm"
+        CM_2_5  = "2-5cm", "2-5cm"
+        CM_5_10 = "5-10cm", "5-10cm"
+        CM_10 = ">10cm", ">10cm"
         
-    site = models.ForeignKey(Site, on_delete=models.RESTRICT)
-    logged_by = models.ForeignKey(User, on_delete=models.RESTRICT)
-    bone = models.ForeignKey(Element, on_delete=models.RESTRICT)
+    # site = models.ForeignKey(Site, on_delete=models.RESTRICT)
+    # logged_by = models.ForeignKey(User, on_delete=models.RESTRICT)
+    element = models.ForeignKey(Element, on_delete=models.RESTRICT)
     side = models.CharField(choices=Side, max_length=20)
     age = models.CharField(choices=Age, max_length=50)
     sex = models.CharField(choices=Sex, max_length=50)
     size = models.CharField(choices=Size, max_length=50)
-    generic = models.BooleanField()
+    generic = models.BooleanField(default=False)
     complete = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
     notes = models.CharField(max_length=2000)
     
     landmarks = models.ManyToManyField(Landmark, through="IDLandmarks")
+
+    class Meta:
+        verbose_name_plural = "entries"
 
 class IDLandmarks(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.RESTRICT)
