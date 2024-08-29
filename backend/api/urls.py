7ded68295655f80c8968adbcc80de92b1103af27
Zhihao Lin <zhi.lin1@outlook.com>
2024-08-29 15:36:32 +0800

    Converted existing views to class based views.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('elements/', views.element_list),
-    path('elements/<str:name>/', views.element_detail),
+    path('elements/', views.ElementList.as_view()),
+    path('elements/<str:name>/', views.ElementDetail.as_view()),
 ]
 
 # Example usage - http http://127.0.0.1:8000/api/elements/ Accept:text/html
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,23 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import Http404
+from rest_framework.views import APIView
 from api.models import Landmark, Element
 from api.serializers import LandmarkSerializer, ElementSerializer
 
 # Create your views here.
 
-@api_view(['GET','POST'])
-def element_list(request, format=None):
-    if request.method == 'GET':
+class ElementList(APIView):
+    def get(self, request, format=None):
         elements = Element.objects.all()
         # Model object -> serializer
         serializer = ElementSerializer(elements, many=True)
         # Serializer -> JSON
         return Response(serializer.data)
-
-    elif request.method == 'POST':
+    
+    def post(self, request, format=None):
         # JSON data -> serializer
+        # DRF request.data automatically parses incoming data.
         serializer = ElementSerializer(data=request.data)
         if serializer.is_valid():
             # serializer -> model object
@@ -24,27 +25,27 @@ def element_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def element_detail(request, name, format=None):
-    try:
-        element = Element.objects.get(name=name)
-    except Element.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class ElementDetail(APIView):
+    def get_object(self, name):
+        try:
+            return Element.objects.get(name=name)
+        except Element.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        # Model object -> serializer
+    def get(self, request, name, format=None):
+        element = self.get_object(name)
         serializer = ElementSerializer(element)
         return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        # DRF request.data automatically parses incoming data.
-        # Updates element with data, returns serializer 
+
+    def put(self, request, name, format=None):
+        element = self.get_object(name)
         serializer = ElementSerializer(element, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, name, format=None):
+        element = self.get_object(name)
         element.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
