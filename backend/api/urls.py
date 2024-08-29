be732fc4c7f82397bfd272179ce05dc57bca25f6
Zhihao Lin <zhi.lin1@outlook.com>
2024-08-29 15:22:17 +0800

    Changed existing views to work with DRF Request and Response objects

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import views
+from api import views
 
 urlpatterns = [
     path('elements/', views.element_list),
     path('elements/<str:name>/', views.element_detail),
 ]
+
+# Example usage - http http://127.0.0.1:8000/api/elements/ Accept:text/html
+# Example usage - http http://127.0.0.1:8000/api/elements.json
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,51 +1,50 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from api.models import Landmark, Element
 from api.serializers import LandmarkSerializer, ElementSerializer
 
 # Create your views here.
 
-@csrf_exempt
-def element_list(request):
+@api_view(['GET','POST'])
+def element_list(request, format=None):
     if request.method == 'GET':
         elements = Element.objects.all()
         # Model object -> serializer
         serializer = ElementSerializer(elements, many=True)
         # Serializer -> JSON
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
         # JSON data -> serializer
-        serializer = ElementSerializer(data=data)
+        serializer = ElementSerializer(data=request.data)
         if serializer.is_valid():
             # serializer -> model object
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
-@csrf_exempt
-def element_detail(request, name):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def element_detail(request, name, format=None):
     try:
         element = Element.objects.get(name=name)
     except Element.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         # Model object -> serializer
         serializer = ElementSerializer(element)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
+        # DRF request.data automatically parses incoming data.
         # Updates element with data, returns serializer 
-        serializer = ElementSerializer(element, data=data)
+        serializer = ElementSerializer(element, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         element.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
