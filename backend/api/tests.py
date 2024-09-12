ae10ba4bcb3cc7ea5894e3d5f6787c1f809e693f
Zhihao Lin <zhi.lin1@outlook.com>
2024-09-12 19:24:12 +0800

    Written tests for auth and entries

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,31 +1,40 @@
-from django.test import TestCase
-from api.models import Element, Landmark, Entry, IDLandmarks
-from django.db.models import RestrictedError
+from django.test import TestCase, Client
+import json
+from django.core.management import call_command
 # Create your tests here.
 
-class FibulaTestCase(TestCase):
-  def setUp(self):
-    Element.objects.create(name="Fibula")
-    fibula = Element.objects.get(name="Fibula")
-    Landmark.objects.create(name="Head", id="FIB-1", bone=fibula)
-    Landmark.objects.create(name="Neck", id="FIB-2", bone=fibula)
-    Landmark.objects.create(name="Shaft", id="FIB-3", bone=fibula)
-    Landmark.objects.create(name="Distal articular surface", id="FIB-4", bone=fibula)
-    Landmark.objects.create(name="Lateral malleolus", id="FIB-5", bone=fibula)
 
-  def test_fibula_elements(self):
-    fibula = Element.objects.get(name="Fibula")
-    fibula_elements = ["Head", "Neck", "Shaft", "Distal articular surface", "Lateral malleolus"]
-    fibula_list = fibula.landmark_set.all()
-    for i in fibula_list:
-      # Get landmark name out of __str__ method
-      landmark_name = str(i).split("-", 1)[0].strip()
-      self.assertIn(landmark_name, fibula_elements)
+class SignupTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.client.post(
+            "/auth/users/", {"username": "testuser", "password": "unCOMMON1234"},)
 
-  def test_delete_landmark_cascade(self):
-    fibula = Element.objects.get(name="Fibula")
-    fibula_list = fibula.landmark_set.all()
-    self.assertEqual(fibula_list.count(), 5)
-    fibula.delete()
-    self.assertEqual(fibula_list.count(), 0)
+    def test_create_user(self):
+        response = self.client.post(
+            "/auth/users/", {"username": "testuser1", "password": "unCOMMON1234"},)
+        self.assertEqual(response.status_code, 201)
 
+    def test_login(self):
+        response = self.client.post(
+            "/auth/token/login", {"username": "testuser", "password": "unCOMMON1234"},)
+        self.assertEqual(response.status_code, 200)
+
+
+class EntryTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.client.post(
+            "/auth/users/", {"username": "testuser", "password": "unCOMMON1234"},)
+        call_command('loaddata', 'elements')
+        call_command('loaddata', 'landmarks')
+
+    def test_create_entry(self):
+        response = self.client.post(
+            "/auth/token/login/", {"username": "testuser", "password": "unCOMMON1234"},)
+        token = response.data["auth_token"]
+        entry = {"acc_num": 1, "bone": 1, "side": "Left", "age": "Adult", "sex": "Female", "size": "2-5cm",
+                 "generic": True, "complete": 2, "landmarks": [4]}
+        response = self.client.post(
+            "/entries/", json.dumps(entry), headers={"Authorization": f"Token {token}"}, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
