71184b73b7678e5e81c24f5b96b1f6ba3230727e
Zhihao Lin <zhi.lin1@outlook.com>
2024-09-30 22:37:43 +0800

    Added new tests for extended models

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -22,31 +22,66 @@ from django.core.management import call_command
 
 
 class EntryTestCase(TestCase):
+    fixtures = ['elements', 'landmarks', 'sites', 'spits']
+
     def setUp(self):
         self.client = Client()
         self.client.post(
             "/auth/users/", {"username": "testuser", "password": "unCOMMON1234", "re_password": "unCOMMON1234"},)
-        call_command('loaddata', 'elements')
-        call_command('loaddata', 'landmarks')
-        call_command('loaddata', 'sites')
-        call_command('loaddata', 'spits')
 
-    def test_create_entryGroup_entry(self):
+    def test_create_entryMeta(self):
         response = self.client.post(
             "/auth/token/login/", {"username": "testuser", "password": "unCOMMON1234"},)
         token = response.data["auth_token"]
-        entryGroup = {
-            "acc_num": 1,
+        entryMeta = {
             "sex": "Female",
             "age": "Infant",
-            "entry_type": "Individual",
             "site": 1,
             "spit": 1
         }
         response = self.client.post(
-            "/entry-groups/", json.dumps(entryGroup), headers={"Authorization": f"Token {token}"}, content_type="application/json")
+            "/entry-meta/", json.dumps(entryMeta), headers={"Authorization": f"Token {token}"}, content_type="application/json")
         self.assertEqual(response.status_code, 201)
         entry = {
+            "meta": response.data["pk"],
+            "acc_num": 1,
+            "bone": 1,
+            "side": "Left",
+            "size": "2-5cm",
+            "generic": True,
+            "complete": 2,
+            "landmarks": [4]
+        }
+        response = self.client.post(
+            "/entries/", json.dumps(entry), headers={"Authorization": f"Token {token}"}, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_body_part(self):
+        response = self.client.post(
+            "/auth/token/login/", {"username": "testuser", "password": "unCOMMON1234"},)
+        token = response.data["auth_token"]
+        entryMeta = {
+            "sex": "Female",
+            "age": "Infant",
+            "site": 1,
+            "spit": 1
+        }
+        response = self.client.post(
+            "/entry-meta/", json.dumps(entryMeta), headers={"Authorization": f"Token {token}"}, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        meta_pk = response.data["pk"]
+
+        body_part = {
+            "bp_code": "BP1",
+            "meta": meta_pk,
+        }
+        response = self.client.post(
+            "/body-parts/", json.dumps(body_part), headers={"Authorization": f"Token {token}"}, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+
+        entry = {
+            "meta": meta_pk,
+            "body_part": "BP1",
             "acc_num": 1,
             "bone": 1,
             "side": "Left",
