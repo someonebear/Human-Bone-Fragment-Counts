e31490227bd496acebaf8fff01f168c7a34686e3
Zhihao Lin <zhi.lin1@outlook.com>
2024-10-09 21:58:24 +0800

    Changed settings again

--- backend/settings.py
+++ backend/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_URL = 'static/'
 
 # Default primary key field type
